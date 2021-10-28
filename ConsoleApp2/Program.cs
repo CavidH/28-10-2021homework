90c2da238cb9eee0de76563630299e8ba8f707af
CavidH <hcavid386@gmail.com>
2021-10-28 11:56:30 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReversSentence("Hello World!"));
+        }
+        static string ReversSentence(string Sentence)
+        {
+            int Flag = 0;
+            string ResultSentence = string.Empty;
+            for (int i = 0; i < Sentence.Length; i++)
+            {
+
+                if (Sentence[i] == '.' || Sentence[i] == ',' || Sentence[i] == ' ' || Sentence[i] == '?' || Sentence[i] == '!' || Sentence[i] == ':')
+                {
+                    ResultSentence += ReverseWord(Sentence, i - 1, Flag) + Sentence[i];
+                    // ResultSentence +=Sentence[i];
+                    Flag = i + 1;
+                }
+            }
+            return ResultSentence;
+        }
+        static string ReverseWord(string Word, int i, int Flag)
+        {
+            string Result = string.Empty;
+            while (i >= Flag)
+            {
+                Result += Word[i];
+                i--;
+            }
+
+            return Result;
+        }
+    }
+}
+
